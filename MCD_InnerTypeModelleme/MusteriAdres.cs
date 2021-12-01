52303864908dcf2d8b14246178310a452bbd6e79
jackup <yk.58@hotmail.com>
2021-12-01 21:21:00 +0300

    Add project files.

---
+++ MCD_InnerTypeModelleme/MusteriAdres.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_InnerTypeModelleme
+{
+    class MusteriAdres
+    {
+        public string adresTip { get; set; }
+        public string Il { get; set; }
+        public string Ilce { get; set; }
+        public string Adres { get; set; }
+
+        public MusteriAdres()
+        {
+            Console.WriteLine("Musteri Adres yapıcı metot çalıştı...");
+        }
+
+        public void MusteriAdresTestMetot()
+        {
+            Console.WriteLine("MusteriAdresTestMetot");
+        }
+    }
+}
---
+++ MCD_InnerTypeModelleme/MusteriIletisimBilgisi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_InnerTypeModelleme
+{
+    class MusteriIletisimBilgisi
+    {
+        public int iletisimTip { get; set; } //717770001 : Mobil - 71777002 : Sabit Hat
+        public string alanKodu { get; set; }
+        public string numara { get; set; }
+        public bool aktifPasif { get; set; }
+
+        public void MusteriIletisimBilgisiTestMetod()
+        {
+            Console.WriteLine("MusteriIletisimTestMetod");
+        }
+    }
+}
