1ba61d967e0a39c34aee97e7f684e657e6bf2d20
jackup <yk.58@hotmail.com>
2021-12-02 16:05:53 +0300

    code changed

--- MCD_InnerTypeModelleme/Program.cs
+++ MCD_InnerTypeModelleme/Program.cs
@@ -10,6 +10,40 @@ namespace MCD_InnerTypeModelleme
     {
         static void Main(string[] args)
         {
+            Musteri M1 = new Musteri();
+            M1.musteriID = 1;
+            M1.tcKimlikNumarasi = "12254401285";
+            M1.isim = "Faik";
+            M1.soyisim = "Turan";
+            M1.olusturmaTarih = DateTime.Now;
+
+            //M1.musteriAdresleri = new MusteriAdres[5]; // Asla yapılmamalı!!!
+
+            M1.musteriAdresleri[0] = new MusteriAdres()
+            {
+                Il = "İstanbul",
+                Ilce = "Kadıköy",
+                Adres = "Adres",
+                adresTip = "İşyeri"
+            };
+
+            M1.musteriAdresleri[0].MusteriAdresTestMetot();
+            M1.musteriSiparisBilgileri[0] = new MusteriSiparisBilgisi()
+            {
+                siparisNumarasi = "SIP0001"
+            };
+
+            M1.musteriSiparisBilgileri[0].urunler[0] = new Urun()
+            {
+                urunId = 1,
+                Tanim = "Telefon",
+                fiyat = 1500
+            };
+
+            foreach (var item in )
+            {
+
+            }
         }
     }
 }
